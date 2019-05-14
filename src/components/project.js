ce1b942f436006bf99158a8eb02d6741366c2ee5
mccaffertycr <mccaffertycr@gmail.com>
2019-05-14 15:11:26 -0400

    added project modals, these could use a little more work to be more responsive

--- src/components/project.js
+++ src/components/project.js
@@ -1,65 +1,126 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 // import Folder from './folder'
 
 import Img from 'gatsby-image'
+import Modal from 'react-responsive-modal'
 
 export const projectScreenshot = graphql`
   fragment projectScreenshot on File {
     childImageSharp {
-      fluid(maxWidth: 150) {
+      fluid(maxWidth: 450) {
         ...GatsbyImageSharpFluid
       }
     }
   }
 `
 
-const Project = ({ name, url, idx }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        placeholderImage1: file(relativePath: { eq: "dsj_screenshot.png" }) {
-          ...projectScreenshot
-        }
-        placeholderImage2: file(
-          relativePath: { eq: "memeoth_screenshot.png" }
-        ) {
-          ...projectScreenshot
-        }
-        placeholderImage3: file(
-          relativePath: { eq: "ssbu-tierlist_screenshot.png" }
-        ) {
-          ...projectScreenshot
-        }
-        placeholderImage4: file(
-          relativePath: { eq: "clickout_screenshot.png" }
-        ) {
-          ...projectScreenshot
-        }
-        placeholderImage5: file(
-          relativePath: { eq: "movie-guess_screenshot.png" }
-        ) {
-          ...projectScreenshot
-        }
-        placeholderImage6: file(relativePath: { eq: "nyt_screenshot.png" }) {
-          ...projectScreenshot
-        }
-        placeholderImage7: file(relativePath: { eq: "rps_screenshot.png" }) {
-          ...projectScreenshot
-        }
-      }
-    `}
-    render={data => {
-      return (
-        <div style={{ width: 180, textAlign: 'center', padding: 10 }}>
-          <Img fluid={data[`placeholderImage${idx}`].childImageSharp.fluid} />
-          <h2 style={{ fontSize: 18, fontWeight: `normal`, color: `white` }}>
-            {name}
-          </h2>
-        </div>
-      )
-    }}
-  />
-)
+class Project extends Component {
+  state = { open: false }
+  toggleModal = e => {
+    e.preventDefault()
+    this.setState({ open: !this.state.open })
+  }
+  render() {
+    const { name, url, idx, desc, tech } = this.props
+    return (
+      <StaticQuery
+        query={graphql`
+          query {
+            placeholderImage1: file(
+              relativePath: { eq: "dsj_screenshot.png" }
+            ) {
+              ...projectScreenshot
+            }
+            placeholderImage2: file(
+              relativePath: { eq: "memeoth_screenshot.png" }
+            ) {
+              ...projectScreenshot
+            }
+            placeholderImage3: file(
+              relativePath: { eq: "ssbu-tierlist_screenshot.png" }
+            ) {
+              ...projectScreenshot
+            }
+            placeholderImage4: file(
+              relativePath: { eq: "clickout_screenshot.png" }
+            ) {
+              ...projectScreenshot
+            }
+            placeholderImage5: file(
+              relativePath: { eq: "movie-guess_screenshot.png" }
+            ) {
+              ...projectScreenshot
+            }
+            placeholderImage6: file(
+              relativePath: { eq: "nyt_screenshot.png" }
+            ) {
+              ...projectScreenshot
+            }
+            placeholderImage7: file(
+              relativePath: { eq: "rps_screenshot.png" }
+            ) {
+              ...projectScreenshot
+            }
+          }
+        `}
+        render={data => {
+          return (
+            <div
+              style={{
+                cursor: 'pointer',
+                width: 180,
+                textAlign: 'center',
+                padding: 10,
+              }}
+            >
+              <a href={'modalDialog'} onClick={this.toggleModal}>
+                <Img
+                  fluid={data[`placeholderImage${idx}`].childImageSharp.fluid}
+                />
+              </a>
+              <h2
+                style={{ fontSize: 18, fontWeight: `normal`, color: `white` }}
+              >
+                {name}
+              </h2>
+              <Modal
+                open={this.state.open}
+                onClose={this.toggleModal}
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  flexDirection: 'column',
+                  maxWidth: 450,
+                }}
+              >
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    flexDirection: 'column',
+                    width: 400,
+                    color: 'black',
+                    height: `70vh`,
+                  }}
+                >
+                  <h2>{name}</h2>
+                  <Img
+                    fluid={data[`placeholderImage${idx}`].childImageSharp.fluid}
+                  />
+                  <a style={{ textDecoration: 'underline' }} href={url}>
+                    Live Demo
+                  </a>
+                  <p>{desc}</p>
+                  <p>Technologies Used: {tech}</p>
+                </div>
+              </Modal>
+            </div>
+          )
+        }}
+      />
+    )
+  }
+}
 
 export default Project
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -9,36 +9,58 @@ const projects = [
     name: `daily sentiment journal`,
     url: `https://daily-sentiment-journal.herokuapp.com/`,
     imgUrl: '/dsj_screenshot',
+    desc: `Daily Sentiment Journal is a simple tool for logging your good and bad days, users are able to make journal entries then that data is visualized on a calendar heatmap similar to Github's contribution calendar`,
+    tech:
+      'JavaScript (ES6+), MongoDB/Mongoose, Node/Express, React, Axios, Google OAuth, Heroku',
   },
   {
     name: `memeoth`,
     url: `http://www.memeoth.com`,
     imgUrl: '/memeoth_screenshot',
+    desc: `Gamified user generated content platform for memes, includes features like a point system and daily event loop that aim to incentive interactivty and engagement with new content.`,
+    tech:
+      'JavaScript (ES6+), MongoDB/Mongoose, Node/Express, React, Axios, Passport, Bootstrap, Cloudinary, node-schedule, Heroku',
   },
   {
     name: `ssbu tierlist`,
     url: `http://ssbutierlist.com`,
     imgUrl: '/ssbu-tierlist_screenshot',
+    desc:
+      'Application built for creating and finding playlists for the game Super Smash Bros. Ultimate, users can easily create new tierlists by dragging and dropping fighters and can sort existing tierlists by High Rated, Recent and Random',
+    tech:
+      'JavaScript (ES6+), MongoDB/Mongoose, Node/Express, React, react-beautiful-dnd, Axios, CSS Flexbox, Heroku',
   },
   {
     name: `click-out!!`,
     url: `https://mccaffertycr.github.io/clicky-game/`,
     imgUrl: '/clickout_screenshot',
+    desc: `Memory game built with React, click on each character once to win, if you click the same character twice it's game over.`,
+    tech: 'JavaScript (ES6+), React, CRA, gh-pages',
   },
   {
     name: `movie guess`,
     url: `https://mccaffertycr.github.io/movie-guess/`,
     imgUrl: '/movie-guess_screenshot',
+    desc:
+      'Movie word guessing game, a random movie is chosen, the data is retrieved from the OMDB API then the user is able to guess what the movie is and get a hint if they are stuck',
+    tech: 'HTML, CSS, JavaScript, jQuery, OMDB API',
   },
   {
     name: `nyt react app`,
     url: `https://nyt-mern-simple.herokuapp.com/`,
     imgUrl: '/nyt_screenshot',
+    desc:
+      'Simple New York Times article search app built with React. Use the search bar to query the NYT Api for new articles and manage them with a MongoDB backend',
+    tech:
+      'Node/Express, React, MongoDB/Mongoose, Axios, Bootstrap, NYT API, Heroku',
   },
   {
     name: `rps multiplayer`,
     url: `https://mccaffertycr.github.io/rps-multiplayer`,
     imgUrl: '/rps_screenshot',
+    desc:
+      'Real time multiplayer rock-paper-scissors, the app allows 2 players to connect and play rock-paper-scissors. There is also a chat and score is kept.',
+    tech: 'HTML, CSS, JavaScript, jQuery, Bootstrap, Firebase',
   },
 ]
 
@@ -56,7 +78,14 @@ const Portfolio = () => (
         }}
       >
         {projects.map((p, i) => (
-          <Project key={i} name={p.name} url={p.url} idx={i + 1} />
+          <Project
+            key={i}
+            name={p.name}
+            url={p.url}
+            desc={p.desc}
+            tech={p.tech}
+            idx={i + 1}
+          />
         ))}
       </div>
     </div>
